c462090c5594652f06cff4934e38ebebcf771ed9
ORESTIS DIMADIS Con <orestis.dimadis.k@gmail.com>
2024-12-17 01:23:20 +0200

    Hotfix

---
+++ src/gr/aueb/cf/ch3/MyCalculator.java
@@ -0,0 +1,18 @@
+package gr.aueb.cf.ch3;
+
+public class MyCalculator {
+
+    public static void main(String[] args) {
+        int num1 = 3;
+        int num2 = 5;
+        int result = 0;
+
+        result = num1*num2;
+
+
+    }
+
+    public static int mul(int a, int b) {
+        return a * b;
+    }
+}
